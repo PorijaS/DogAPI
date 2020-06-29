4bfdd51d7fbd5ef65fc437b6e92f3a672594d4e9
PorijaS <48723207+PorijaS@users.noreply.github.com>
2020-06-29 10:19:36 +0200

    Added the API and other Javescripts

---
+++ js/dogapi.js
@@ -0,0 +1,133 @@
+//GLOBAL VARIABLES
+const dogList = document.getElementById("doggoDropDown");
+const listButton = document.querySelector(".list");
+//spinner
+
+//EVENT LISTENERS
+// window load
+window.addEventListener("load", populateList);
+//click and change
+dogList.addEventListener("change", getBreedImage);
+listButton.addEventListener("click", getBreedImage);
+
+
+//random dog image
+function getRandomDoggo() {
+  fetch("https://dog.ceo/api/breeds/image/random")
+    .then(checkStatus)
+    .then((response) => response.json())
+    .then((data) => handleData(data))
+    .catch((error) => notifyUser(error));
+}
+
+//populate List
+function populateList() {
+  fetch("https://dog.ceo/api/breeds/list/all")
+    .then(checkStatus)
+    .then((response) => response.json())
+    .then((data) => createListItems(data.message))
+    .catch((error) => notifyUser(error));
+}
+
+//getBreedImage
+function getBreedImage() {
+  //get list value
+  var selectedBreed = dogList.options[dogList.selectedIndex].value;
+  //build url
+  var url = `https://dog.ceo/api/breed/${selectedBreed}/images`;
+  //fetch call
+  fetch(url)
+    .then(checkStatus)
+    .then((response) => response.json())
+    .then((data) => getImageURL(data.message))
+    .catch((error) => console.log(error));
+}
+
+
+//HELPER FUNCTIONS
+//checkStatus
+function checkStatus(response) {
+  if (response.ok) {
+    return Promise.resolve(response);
+  } else {
+    return Promise.reject(new Error(response.statusText));
+  }
+}
+
+
+//handleData
+function handleData(data) {
+  var url = data.message;
+  console.log(url);
+  var regex = /https:\/\/images\.dog\.ceo\/breeds\/(\w+\-?\w+)\/.+/g;
+  var breedName = regex.exec(url);
+  document.getElementById(
+    "randomImageContainer"
+  ).innerHTML = `<img alt="random image of a ${fixBreed(
+    breedName[1]
+  )}" src='${url}'/>`;
+  document.querySelector(
+    ".dogInfo"
+  ).innerHTML = `<p class="h5">Random image of a ${fixBreed(breedName[1])}</p>`;
+}
+
+//getImageURL
+function getImageURL(data) {
+  //get random number
+  var randomNumberURL = data[Math.floor(Math.random() * data.length) + 1];
+  listImageContainer.innerHTML = `<img src="${randomNumberURL}" alt="${extractBreedName(
+    data
+  )}"/>`;
+}
+
+//createListItems
+function createListItems(data) {
+  var output = "";
+  Object.keys(data).forEach(
+    (key) => (output += `<option value="${key}">${fixBreed(key)}</option>`)
+  );
+  document.getElementById("doggoDropDown").innerHTML = output;
+}
+
+//notifyUser
+function notifyUser(error) {
+  const errorContainer = document.querySelector(".alert");
+  errorContainer.innerHTML = `There was an error with the server request (${error}). <br> Click the button again.`;
+  errorContainer.style.display = "block";
+  setTimeout(() => {
+    errorContainer.innerHTML = "";
+    errorContainer.style.display = "none";
+  }, 4000);
+}
+
+//fixBreed
+function fixBreed(breedName) {
+  if (breedName === "germanshepherd") {
+    return "German Shepherd";
+  } else if (breedName === "mexicanhairless") {
+    return "Mexican Hairless";
+  } else if (breedName === "stbernard") {
+    return "St. Bernard";
+  } else if (breedName === "african") {
+    return "African Wild Dog";
+  } else if (breedName === "bullterrier") {
+    return "Bull Terier";
+  }
+  return capitalize(breedName);
+}
+
+//capitalize breed name
+function capitalize(breedName) {
+  return breedName
+    .replace(/\-/g, " ")
+    .split(" ")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
+}
+
+//extract breed name
+function extractBreedName(data) {
+  var regex = /https:\/\/images\.dog\.ceo\/breeds\/(\w+-?\w+)\/\w+\.\w+/g;
+  var match = regex.exec(data);
+  return fixBreed(match[1]);
+}
